03539b2f18e660826ff924f77a40d3a69c418c99
DkorTk <1294956265@qq.com>
2020-12-07 22:35:30 +0800

    token存储完成

--- controllers/user/login.js
+++ controllers/user/login.js
@@ -1,20 +1,28 @@
 const { query } = require("../../lib/db");
+const qs = require("qs");
+const jwt = require("jsonwebtoken");
 module.exports = async ctx => {
-    const {email, password} = ctx.request.body;
+
+    const {email, password} = JSON.parse(ctx.request.body.data);
     const sql = `SELECT * FROM user WHERE email=? AND password=?`;
     const [row] = await query(sql, [email, password]);
     const userInfo = row;
+    console.log(email, password);
+    // console.log(row.id);
     if (userInfo) {
       //登录成功
-    //   const token = jsonwebtoken.sign({ uId: userInfo.id }, SECRET, {
-    //     expiresIn: "2h"
-    //   });
+
+      // token
+      const token = jwt.sign({ uId: userInfo.id }, 'ldk', {
+        expiresIn: "500"
+      });
+      console.log(token);
       ctx.body = {
         state: 1,
         code: 200,
-        // data: {
-        //   token
-        // }
+        msg: "登录成功",
+        token
+        
       };
     } else {
       //登录失败
@@ -24,4 +32,5 @@ module.exports = async ctx => {
         msg: "登录失败"
       };
     }
+
 }
